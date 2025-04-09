f9602c078698bee69d064fac4eb71404c792af02
Lucas Lima <lucasfsl@al.insper.edu.br>
2025-04-09 11:43:22 -0300

    Adicionado .env.

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import Knex from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('Database not found')
+}
 
 export const config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
